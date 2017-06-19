0b6ff84c4b7da7eb8b7aded109260e6e9ae0a1c6
Adam Lindberg <hello@alind.io>
2017-06-19 18:55:33 +0200

    First version of release version checking

--- src/rebar3_grisp_deploy.erl
+++ src/rebar3_grisp_deploy.erl
@@ -30,8 +30,31 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
+    Config = rebar_state:get(State, grisp, []),
+    check_otp_release(Config),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
+
+%--- Internal ------------------------------------------------------------------
+
+check_otp_release(Config) ->
+    case proplists:get_value(otp_release, Config) of
+        undefined ->
+            rebar_api:abort(
+                "GRiSP OTP release in rebar.config not configured:"
+                "~n~n{grisp, [{otp_release, \"<VERSION>\"}]}", []
+            );
+        OTPRelease ->
+            case {OTPRelease, erlang:system_info(otp_release)} of
+                {Target, Target} -> ok;
+                {Target, Current} ->
+                    rebar_api:warn(
+                        "Current Erlang version (~p) does not match target"
+                        "Erlang version (~p). It is not guaranteed that the"
+                        "deployed release will work!", [Current, Target]
+                    )
+                end
+    end.
