d79013688b22f6c0c853d788f61e1f5df654cfde
Adam Lindberg <hello@alind.io>
2018-09-11 16:27:09 +0200

    Start dependencies
    Rebar 3 doesn't start dependencies of plug-ins by default, so we have to
    do it manually.

--- src/rebar3_grisp.erl
+++ src/rebar3_grisp.erl
@@ -7,6 +7,11 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
+    % Start applications, because Rebar 3 doesn't do it for us
+    {ok, AllDeps} = application:get_key(?MODULE, applications),
+    Deps = AllDeps -- [kernel, stdlib],
+    [{ok, _} = application:ensure_all_started(A) || A <- Deps],
+    % Register tasks
     lists:foldl(fun(Mod, {ok, S}) -> Mod:init(S) end, {ok, State}, [
         rebar3_grisp_deploy,
         rebar3_grisp_build,
